f8bde76145e39fc32754abcf3c7ed3847e62b9dc
sigma-Jakub <jakub.mazur@student.nhlstenden.com>
2026-02-11 14:55:40 +0100

    Update motor pin assignments in klankerMovement

--- klankerMovement/klankerMovement.ino
+++ klankerMovement/klankerMovement.ino
@@ -1,8 +1,8 @@
 const int LeftForward = 11;
 const int LeftBackward = 10;
 const int RightBackward = 3;
-const int RightForward = 5;
-const int RotatorOne = 9;
+const int RightForward = 8;
+const int RotatorOne = 5;
 const int RotatorTwo = 6;
 
 void setup() {
