db07741d901c83758f4abc51e910810a403d5b39
Jakub <jakub.mazur@student.nhlstenden.com>
2026-03-06 10:23:10 +0100

    updated follow the line exercise

--- jakub assignments/lineFollowWithArrays/lineFollowWithArrays.ino
+++ jakub assignments/lineFollowWithArrays/lineFollowWithArrays.ino
@@ -4,8 +4,6 @@ const int BACK_LEFT = 11;
 const int BACK_RIGHT = 10;
 const int ECHO_PIN = 7;
 const int TRIGGER_PIN = 12;
-const int ENCODER_LEFT = 2;
-const int ENCODER_RIGHT = 3;
 
 const int SENSOR_PINS[] = {A0, A1, A2, A4, A3, A5, A6, A7};
 int s[8];
@@ -14,24 +12,14 @@ const int threshold = 480;
 const int sensorStopValue = 900;
 int baseSpeed = 215;
 
-volatile long rotationCounterLeft = 0;
-volatile long rotationCounterRight = 0;
-float errorIntegral = 0;
-
 void setup() {
   Serial.begin(9600);
-  
   pinMode(FORWARD_LEFT, OUTPUT);
   pinMode(FORWARD_RIGHT, OUTPUT);
   pinMode(BACK_LEFT, OUTPUT);
   pinMode(BACK_RIGHT, OUTPUT);
   pinMode(ECHO_PIN, INPUT_PULLUP);
   pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ENCODER_LEFT, INPUT_PULLUP);
-  pinMode(ENCODER_RIGHT, INPUT_PULLUP);
-  
-  attachInterrupt(digitalPinToInterrupt(ENCODER_LEFT), countLeft, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_RIGHT), countRight, RISING);
 }
 
 void loop() {  
@@ -49,6 +37,8 @@ void loop() {
 //  Serial.println(s[7]);
 //  Serial.println("__________");
 //  delay(2000);
+//  black = 1000
+// white = 200 - 450
 
     if(s[0] > sensorStopValue && s[1] > sensorStopValue && s[2] > sensorStopValue && s[3] > sensorStopValue && 
        s[4] > sensorStopValue && s[5] > sensorStopValue && s[6] > sensorStopValue && s[7] > sensorStopValue){
@@ -57,7 +47,7 @@ void loop() {
     else if((s[3] > threshold && s[4] > threshold) || 
        (s[1] > threshold && s[2] > threshold && s[3] > threshold && s[4] > threshold) || 
        (s[3] > threshold && s[4] > threshold && s[5] > threshold && s[6] > threshold && s[7] > threshold)) {
-      balanceMotors(baseSpeed + 55, true);
+      moveMotors(baseSpeed, baseSpeed);
     }
     else if (s[2] > threshold && s[3] > threshold) {
       moveMotors(baseSpeed + 20, baseSpeed - 20);
@@ -78,54 +68,6 @@ void loop() {
       moveMotors(0, baseSpeed + 55); 
     }
   }
-
-void countLeft(){
-  rotationCounterLeft++;
-}
-
-void countRight(){
-  rotationCounterRight++;  
-}
-
-void balanceMotors(int baseSpeed, bool forward) {
-  const float PROPORTIONAL_GAIN = 15;
-  const float INTEGRAL_GAIN = 0.05;
-  const float DERIVATIVE_GAIN = 0.3;
-  
-  const float MAX_INTEGRAL = 150.0;
-
-  int lastDifference = 0;
-
-  int difference = rotationCounterLeft - rotationCounterRight;
-
-  errorIntegral += difference;
-  errorIntegral = constrain(errorIntegral, -MAX_INTEGRAL, MAX_INTEGRAL);
-  
-  int derivative = difference - lastDifference;
-  lastDifference = difference;
-
-  float correction = (PROPORTIONAL_GAIN * difference) + (INTEGRAL_GAIN * errorIntegral) + (DERIVATIVE_GAIN * derivative);
-
-  int speedLeft = baseSpeed - correction;
-  int speedRight = baseSpeed + correction;
-
-  speedLeft = constrain(speedLeft, 0, 255);
-  speedRight = constrain(speedRight, 0, 255);
-
-  if (forward) {
-    analogWrite(FORWARD_LEFT, speedLeft);
-    analogWrite(BACK_LEFT, 0);
-    analogWrite(FORWARD_RIGHT, speedRight);
-    analogWrite(BACK_RIGHT, 0);
-  } else {
-    analogWrite(FORWARD_LEFT, 0);
-    analogWrite(BACK_LEFT, speedLeft);
-    analogWrite(FORWARD_RIGHT, 0);
-    analogWrite(BACK_RIGHT, speedRight);
-  }
-  
-  delay(10); 
-}
 
 void moveMotors(int left, int right) {
   analogWrite(FORWARD_LEFT, constrain(left, 0, 255));
